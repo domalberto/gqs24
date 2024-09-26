ad4e28aee0b4f9b69f1e967040702f4a7ae0f49f
Bruno TS <108018103+BrunoT-S@users.noreply.github.com>
2024-09-26 15:12:54 -0300

    Atualização design e usuabilidade

--- brunots/script/app.js
+++ brunots/script/app.js
@@ -2,23 +2,39 @@ const prevButton = document.querySelector('.prev');
 const nextButton = document.querySelector('.next');
 const slides = document.querySelectorAll('.carousel-slide');
 let currentIndex = 0;
+const totalSlides = slides.length - 1; // Ajuste para lidar com o índice correto (0 a totalSlides)
 
 function showSlide(index) {
     slides.forEach((slide, i) => {
-        slide.style.transform = `translateX(${(i - index) * 100}%)`;
+        slide.style.transform = `translateX(${(-index) * 100}%)`;
         slide.setAttribute('aria-hidden', i !== index);
     });
 }
 
 nextButton.addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % slides.length;
+    currentIndex = (currentIndex + 1);
+    if (currentIndex > totalSlides) {
+        currentIndex = 0; // Reinicia ao primeiro slide
+    }
     showSlide(currentIndex);
 });
 
 prevButton.addEventListener('click', () => {
-    currentIndex = (currentIndex - 1 + slides.length) % slides.length;
+    currentIndex = (currentIndex - 1);
+    if (currentIndex < 0) {
+        currentIndex = totalSlides; // Volta ao último slide
+    }
     showSlide(currentIndex);
 });
 
 // Exibir o slide inicial
 showSlide(currentIndex);
+
+function togglePeriodo(id) {
+    var content = document.getElementById(id);
+    if (content.style.display === "block") {
+        content.style.display = "none";
+    } else {
+        content.style.display = "block";
+    }
+}
