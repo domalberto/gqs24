7c4d1b6ce0be68376ec45d97d02605a840476c60
Bruno TS <108018103+BrunoT-S@users.noreply.github.com>
2024-09-26 18:11:26 -0300

    Alterações funcionalidade

--- brunots/script/app.js
+++ brunots/script/app.js
@@ -1,34 +1,34 @@
-const prevButton = document.querySelector('.prev');
-const nextButton = document.querySelector('.next');
-const slides = document.querySelectorAll('.carousel-slide');
-let currentIndex = 0;
-const totalSlides = slides.length - 1; // Ajuste para lidar com o índice correto (0 a totalSlides)
+let slideIndex = 1;
+showSlides(slideIndex);
 
-function showSlide(index) {
-    slides.forEach((slide, i) => {
-        slide.style.transform = `translateX(${(-index) * 100}%)`;
-        slide.setAttribute('aria-hidden', i !== index);
-    });
+// Controles para próximo/anterior
+function plusSlides(n) {
+    showSlides(slideIndex += n);
 }
 
-nextButton.addEventListener('click', () => {
-    currentIndex = (currentIndex + 1);
-    if (currentIndex > totalSlides) {
-        currentIndex = 0; // Reinicia ao primeiro slide
-    }
-    showSlide(currentIndex);
-});
+// Controle para selecionar slide específico
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
 
-prevButton.addEventListener('click', () => {
-    currentIndex = (currentIndex - 1);
-    if (currentIndex < 0) {
-        currentIndex = totalSlides; // Volta ao último slide
+// Função para exibir os slides
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
+    let dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
     }
-    showSlide(currentIndex);
-});
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
 
-// Exibir o slide inicial
-showSlide(currentIndex);
+
 
 function togglePeriodo(id) {
     var content = document.getElementById(id);
