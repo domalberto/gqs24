2315595dd83d8feb12d102360f319b5d4e46e117
Christianthh <christianthkk@gmail.com>
2024-09-28 15:22:10 -0300

    grudiando js e ibagens

---
+++ christian/scripts.js
@@ -0,0 +1,17 @@
+function toggleGradeCurricular() {
+    var grade = document.getElementById('grade-Curricular');
+    if (grade.style.display === 'none') {
+        grade.style.display = 'block';
+        fetch('index.html')
+            .then(response => response.text())
+            .then(data => {
+            var parser = new DOMParser();
+            var doc = parser.parseFromString(data, 'text/html');
+            var content = doc.getElementById('grade-Curricular-content').innerHTML;
+            grade.innerHTML = content;
+            })
+            .catch(error => console.error('Error fetching content:', error));
+    } else {
+        grade.style.display = 'none';
+    }
+}
