26329da7dda63d3a573269e0000fc4bd9aa6d140
Jonatas Gollmann <86964636+JonatasGollmann@users.noreply.github.com>
2024-11-18 10:07:36 -0300

    V1.1.11 - Mantendo só a build pra ver se funciona.

--- jonatas/src/components/buttons/ButtonHeader.tsx
+++
@@ -1,8 +0,0 @@
-export default function ButtonHeader({ text, site }: { text:string,site: string }) {
-  return (
-    <a href={site} className="h-10 mx-1 font-medium text-blue_4 px-5 py-3 hover:bg-white_2 hover:text-blue_3 transition-all ease-in-out hover:scale-105 rounded-lg">
-      {text}
-    </a>
-  );
-}
-      
